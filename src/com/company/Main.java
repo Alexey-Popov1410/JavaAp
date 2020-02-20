8a3bda43f04b93bd4021c5393b02eae9cc72a9fc
Алексей <shteindno@gmail.com>
2020-02-20 18:04:30 +0300

    lol

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,17 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int day = scan.nextInt();
+        System.out.print("Введите месяц: ");
+        int month = scan.nextInt();
+        System.out.print("Введите год: ");
+        int year = scan.nextInt();
+        System.out.print("Ваша дата: " + day + "." + month + "." + year);
 
         }
     }
